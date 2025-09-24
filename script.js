1e03d9658b9c46ee34a12d8f47bba7e26820601c
paninishah <paninishah29@gmail.com>
2025-09-24 17:08:55 +0530

    add bootstrap, add typed

---
+++ script.js
@@ -0,0 +1,8 @@
+// typed effect
+var typed = new Typed("#typed", {
+    strings: ["read", "explore", "discover", "escape", "dream"],
+    typeSpeed: 150,
+    backSpeed: 100,
+    backDelay: 1200,
+    loop: true
+});
